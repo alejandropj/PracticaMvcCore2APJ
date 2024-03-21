54cd3bbe3145ae1670379dc0567ca213b1b29afc
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-21 10:57:25 +0100

    11:00

--- Controllers/LibrosController.cs
+++ Controllers/LibrosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using PracticaMvcCore2APJ.Models;
 using PracticaMvcCore2APJ.Repositories;
 
@@ -7,12 +8,29 @@ namespace PracticaMvcCore2APJ.Controllers
     public class LibrosController : Controller
     {
         private RepositoryLibros repo;
-        public LibrosController(RepositoryLibros repo)
+        private IMemoryCache memoryCache;
+        public LibrosController(RepositoryLibros repo, IMemoryCache memoryCache)
         {
             this.repo = repo;
+            this.memoryCache = memoryCache;
         }
-        public async Task<IActionResult> List()
+        public async Task<IActionResult> List(int? idCarrito)
         {
+            if (idCarrito != null)
+            {
+                List<Libro> librosCarrito;
+                if(this.memoryCache.Get("CARRITO") == null)
+                {
+                    librosCarrito = new List<Libro>();
+                }
+                else
+                {
+                    librosCarrito = this.memoryCache.Get<List<Libro>>("CARRITO");
+                }
+                Libro libro = await this.repo.FindLibroById(idCarrito.Value);
+                librosCarrito.Add(libro);
+                this.memoryCache.Set("CARRITO", librosCarrito);
+            }
             List<Libro> libros = await this.repo.GetLibrosAsync();
             return View(libros);
         }        
@@ -25,6 +43,16 @@ namespace PracticaMvcCore2APJ.Controllers
         {
             Libro libro = await this.repo.FindLibroById(idLibro);
             return View(libro);
+        }
+
+        public async Task<IActionResult> CarritoLibros(int? idEliminar)
+        {
+            if (idEliminar != null)
+            {
+                List<Libro> libros = 
+                    this.memoryCache.Get<List<Libro>>("CARRITO");
+                Libro libro = libros.Find(z => z.IdLibro == idEliminar.Value);
+            }
         }
     }
 }
---
+++ Controllers/ManagedController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using PracticaMvcCore2APJ.Models;
+using PracticaMvcCore2APJ.Repositories;
+using System.Security.Claims;
+
+namespace PracticaMvcCore2APJ.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private RepositoryLibros repo;
+        public ManagedController(RepositoryLibros repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            Usuario usuario = await this.repo.LogInUsuarioAsync(email, password);
+            if(usuario != null)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    ClaimTypes.Name, ClaimTypes.Role);
+
+                Claim claimName = new Claim(ClaimTypes.Name, usuario.Nombre);
+                identity.AddClaim(claimName);
+                Claim claimId = new Claim(ClaimTypes.NameIdentifier, usuario.IdUsuario.ToString());
+                identity.AddClaim(claimId);
+                Claim claimApellidos = new Claim(ClaimTypes.Role, usuario.Apellidos);
+                identity.AddClaim(claimApellidos);
+                Claim claimEmail = new Claim("Email", usuario.Email);
+                identity.AddClaim(claimEmail);
+                Claim claimFoto = new Claim("Foto", usuario.Foto);
+                identity.AddClaim(claimFoto);
+
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+
+                string controller = TempData["controller"].ToString();
+                string action = TempData["action"].ToString();
+                return RedirectToAction(action, controller);
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult ErrorAcceso()
+        {
+            return View();
+        }
+    }
+}
